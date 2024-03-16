a2783256a58695ca6ea6e494c14f1ffd39c513c8
l-Luna <izzeldeen03@gmail.com>
2024-03-16 12:50:43 +0000

    take puzzle & solution file from cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs::File;
 use std::io::Read;
 use crate::parse::{parse_puzzle, parse_solution};
@@ -6,15 +7,17 @@ mod parse;
 mod data;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut buffer: Vec<u8> = Vec::new();
-    let _ = File::open("./OM2024_W4_Dental_Amalgam.puzzle").unwrap().read_to_end(&mut buffer).unwrap();
+    let _ = File::open(&args[1]).unwrap().read_to_end(&mut buffer).unwrap();
     let puzzle = parse_puzzle(buffer.as_slice()).unwrap();
     println!("it's like {:?}", puzzle);
 
     println!("and then!!");
 
     let mut buffer2: Vec<u8> = Vec::new();
-    let _ = File::open("./dental-amalgam-OM2024_W4_Dental_Amalgam-34.solution").unwrap().read_to_end(&mut buffer2).unwrap();
+    let _ = File::open(&args[2]).unwrap().read_to_end(&mut buffer2).unwrap();
     let sol = parse_solution(buffer2.as_slice()).unwrap();
     println!("they're like {:?}", sol);
 }
